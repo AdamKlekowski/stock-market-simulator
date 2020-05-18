9863ced3c5854d3fb00b33875953ce3f34cf6006
adamklekowski <klekowskiadam@gmail.com>
2020-05-18 13:54:02 +0200

    add different plots

--- src/Plot/Plot.py
+++ src/Plot/Plot.py
@@ -5,9 +5,27 @@ class Plot:
     @staticmethod
     def displayPlot(cb):
         for name in ["IBM", "ABB"]:
+            Plot.oneStockOnePlot(cb, name)
+        Plot.allStockOnePlot(cb)
+
+    @staticmethod
+    def allStockOnePlot(cb):
+        plt.clf()
+        for name in ["IBM", "ABB"]:
             plt.plot(cb.getPriceListing(name))
         plt.title("Stock prices")
         plt.legend(["IBM", "ABB"])
         plt.ylabel("Price [$]")
         plt.xlabel("Time [s]")
         plt.savefig("Plot/all_stock.png")
+
+    @staticmethod
+    def oneStockOnePlot(cb, stock_name):
+        plt.clf()
+        plt.plot(cb.getPriceListing(stock_name))
+        plt.title("Stock prices for " + str(stock_name))
+        plt.legend([stock_name])
+        plt.ylabel("Price [$]")
+        plt.xlabel("Time [s]")
+        plt.savefig("Plot/" + str(stock_name) + ".png")
+
