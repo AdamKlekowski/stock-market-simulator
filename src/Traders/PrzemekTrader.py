28aa4cfe43df4e222e1d8b54eb82fa971b40a43d
adamklekowski <klekowskiadam@gmail.com>
2020-05-18 13:37:59 +0200

    move cvs/h5 files to data

--- src/Traders/PrzemekTrader.py
+++ src/Traders/PrzemekTrader.py
@@ -9,8 +9,8 @@ class PrzemekTrader(Trader):
     def __init__(self, threadID, cb, orderBook, delay, money):
         Trader.__init__(self, threadID, cb, orderBook, delay, money)
 
-        self.model = tf.keras.models.load_model('mod.h5')
-        self.aapl = np.genfromtxt('AAPL.csv', delimiter=',')
+        self.model = tf.keras.models.load_model('data/mod.h5')
+        self.aapl = np.genfromtxt('data/AAPL.csv', delimiter=',')
         self.aapl = self.aapl[1:, 1:]
         self.scaler = MinMaxScaler(feature_range=(0, 1))
         self.scaler.fit(np.reshape(self.aapl[:, 3], (-1, 1)))
@@ -18,15 +18,15 @@ class PrzemekTrader(Trader):
     def playOnStock(self):
         self.cb.lock.acquire()
         try:
-            self.prIBM=self.cb.stock_exchange_listing['IBM'][-1]
-            self.x=[]
-            for i in range(5,0,-1):
+            self.prIBM = self.cb.stock_exchange_listing['IBM'][-1]
+            self.x = []
+            for i in range(5, 0, -1):
                 self.x.append(self.cb.stock_exchange_listing['IBM'][-i])
-            self.x=[self.scaler.transform([[i]]) for i in self.x]
-            self.x=np.array(self.x)
-            self.x=self.x.reshape((1,1,-1))
-            self.valIBM=self.scaler.inverse_transform(self.model.predict(self.x))[0,0]
-            if self.valIBM>self.prIBM:
+            self.x = [self.scaler.transform([[i]]) for i in self.x]
+            self.x = np.array(self.x)
+            self.x = self.x.reshape((1,1,-1))
+            self.valIBM = self.scaler.inverse_transform(self.model.predict(self.x))[0,0]
+            if self.valIBM > self.prIBM:
                 order = Order(self.threadID, self.threadID, 4, self.prIBM+np.random.normal(0,0.1))
                 self.orderBook["IBM"].addOrder("BID", order)
             else:
